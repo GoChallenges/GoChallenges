7d665cec04e0a3295b0f0a1e0fc01def59571858
Han Nguyen <h.bnguyen2001@gmail.com>
2020-06-01 00:18:12 -0400

    Add segues to signing and register screens

---
+++ GoChallenges/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  GoChallenges
+//
+//  Created by nguyen thy on 6/1/20.
+//  Copyright © 2020 Han Nguyen. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let toRegister = "toRegister"
+    static let toSignin = "toSignin"
+}
