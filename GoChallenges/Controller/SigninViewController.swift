ff99d3082b79572e996fe9d7320055ba3f2f2e10
Jayho261 <phucho2001@gmail.com>
2020-06-22 19:43:03 -0400

    Added auto layout for login screen

--- GoChallenges/Controller/SigninViewController.swift
+++ GoChallenges/Controller/SigninViewController.swift
@@ -7,11 +7,43 @@ import Firebase
 class SigninViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        emailTextField.layer.cornerRadius = 10
+        passwordTextField.layer.cornerRadius = 10
+        emailTextField.layer.masksToBounds = true
+        passwordTextField.layer.masksToBounds = true
+        //emailTextField.layer.borderWidth = 2.0
+        loginButton.layer.cornerRadius = 13
+        //let passwordImage = UIImage(named: "icons8-lock-100 1")
+        //addLeftImage(txtField: passwordTextField, img: passwordImage!)
+       
 
     }
+    @IBOutlet var heightConstraint : NSLayoutConstraint!
     
+    // The code to change the height constraint of the logo image when in landscape mode
+   override func willRotate(to toInterfaceOrientation: UIInterfaceOrientation, duration: TimeInterval){
+        if toInterfaceOrientation == .landscapeLeft || toInterfaceOrientation == .landscapeRight{
+            // Imageview height constraint outlate
+            heightConstraint.constant = 0
+        }
+        else{
+            heightConstraint.constant = 232
+            
+        }
+    }
+    /*
+    func addLeftImage(txtField: UITextField, img: UIImage){
+        let leftImageView = UIImageView()
+        leftImageView.frame = CGRect(x: 0, y: 0, width: 30, height: 40)
+        leftImageView.image = img
+        txtField.leftView = leftImageView
+        txtField.leftViewMode = .always
+    }
+    */
     @IBAction func signIn(_ sender: UIButton) {
         if let email = emailTextField.text, let password = passwordTextField.text{
             Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
@@ -32,16 +64,5 @@ class SigninViewController: UIViewController {
             }
         }
     }
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
