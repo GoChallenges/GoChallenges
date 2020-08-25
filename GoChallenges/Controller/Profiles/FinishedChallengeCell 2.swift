80fe185e9887b8e5d8c0cba5fc9f0004ff5e2492
Han Nguyen <h.bnguyen2001@gmail.com>
2020-08-24 23:09:30 -0400

    Cells

---
+++ GoChallenges/Controller/Profiles/FinishedChallengeCell 2.swift
@@ -0,0 +1,24 @@
+//
+//  FinishedChallengeCell.swift
+//  GoChallenges
+//
+//  Created by nguyen thy on 8/21/20.
+//  Copyright © 2020 Han Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class FinishedChallengeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
